413064fd58cae65444937eb3769d2a5faacdd71c
tanji1804 <ronkinnatsu@icloud.com>
2023-04-04 14:43:41 +0000

    create controllers

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // index
+    public function index()
+    {
+        
+    }
+    
+    // add book
+    public function add()
+    {
+        
+    }
+    
+    //  edit book
+    public function edit()
+    {
+        
+    }
+    
+    // delete book
+    public function delete()
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WordController extends Controller
+{
+    // add word
+    public function add()
+    {
+        
+    }
+    
+    // edit word
+    public function edit()
+    {
+        
+    }
+    
+    // delete word
+    public function delete()
+    {
+        
+    }
+}
