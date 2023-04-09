6132ab75b8bc3bbcac98ad1126f5aad45272ae47
tanji1804 <ronkinnatsu@icloud.com>
2023-04-09 11:55:20 +0000

    edit create action

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BookController extends Controller
 {
+    // add book
+    public function add()
+    {
+        return view('book.create');
+    }
+    
+    // create book
+    public function create(Request $request)
+    {
+        
+    }
+    
     //  edit book
     public function edit()
     {
