ac9cc40dbfab4835eec12cd1a0fc79c28183ac44
tanji1804 <ronkinnatsu@icloud.com>
2023-04-11 12:37:18 +0000

    add, book create popup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,31 @@
 
 @section('title',config('app.name','VocaBoost'))
 @section('content')
-    welcome
+    <script>
+ 
+function confirm_test() { // 問い合わせるボタンをクリックした場合
+    document.getElementById('popup').style.display = 'block';
+    return false;
+}
+ 
+function okfunc() { // OKをクリックした場合
+    document.contactform.submit();
+}
+ 
+function nofunc() { // キャンセルをクリックした場合
+    document.getElementById('popup').style.display = 'none';
+}
+</script>
+<div >
+<form method="POST" name="contactform" action="test2.php">
+    <input type="submit" value="+" name="contact" onclick="return confirm_test()" />
+</form>
+ 
+<div id="popup" style="width: 200px;display: none;padding: 30px 20px;border: 2px solid #000;margin: auto;">
+    <br />
+    <input type="text" name="user_name" value="" />
+    <button id="ok" onclick="okfunc()">OK</button>
+    <button id="no" onclick="nofunc()">キャンセル</button>
+</div>
     
 @endsection
