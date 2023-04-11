bea20d848136cdb36e7959d55b13109567200bd6
tanji1804 <ronkinnatsu@icloud.com>
2023-04-11 13:08:40 +0000

    add auth to top page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title',config('app.name','VocaBoost'))
+@section('title',config('app.name','VocaBoost').' '.__('messages.top'))
 @section('content')
     <script>
  
@@ -17,16 +17,19 @@ function nofunc() { // キャンセルをクリックした場合
     document.getElementById('popup').style.display = 'none';
 }
 </script>
-<div >
-<form method="POST" name="contactform" action="test2.php">
-    <input type="submit" value="+" name="contact" onclick="return confirm_test()" />
-</form>
- 
-<div id="popup" style="width: 200px;display: none;padding: 30px 20px;border: 2px solid #000;margin: auto;">
-    <br />
-    <input type="text" name="user_name" value="" />
-    <button id="ok" onclick="okfunc()">OK</button>
-    <button id="no" onclick="nofunc()">キャンセル</button>
-</div>
-    
+@auth
+    <div >
+    <form method="POST" name="contactform" action="{{ route('book.create') }}">
+        @csrf
+        <input type="submit" value="+" name="contact" onclick="return confirm_test()" />
+    </form>
+     
+    <div id="popup" style="width: 200px;display: none;padding: 30px 20px;border: 2px solid #000;margin: auto;">
+        <br />
+        <input type="text" name="user_name" value="" />
+        <button id="ok" onclick="okfunc()">OK</button>
+        <button id="no" onclick="nofunc()">キャンセル</button>
+    </div>
+@endauth
+
 @endsection
