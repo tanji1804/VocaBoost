e08fceb4468cea5c8762c1c479c9cdb175c5eaa2
tanji1804 <ronkinnatsu@icloud.com>
2023-04-11 15:09:04 +0000

    save to books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Models\Book;
+
 
 class BookController extends Controller
 {
@@ -16,6 +19,22 @@ class BookController extends Controller
     // create book
     public function create(Request $request)
     {
+        $this->validate($request, Book::$rules);
+        
+        $book = new Book;
+        $form = $request->all();
+        $form += array('user_id' => Auth::id());
+        
+        // if($form['book_name'] === null){
+        //     $form['book_name'] = 'untitled';
+        // }
+        
+        
+        unset($form['_token']);
+        // dd($form);
+        $book->fill($form);
+        $book->save();
+        
         return redirect('/');
     }
     
