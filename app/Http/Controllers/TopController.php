023e23cc9070d316dd465aeff9058f220c9f6221
tanji1804 <ronkinnatsu@icloud.com>
2023-04-13 12:36:55 +0000

    success　display all_books

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Book;
 
 class TopController extends Controller
 {
-    public function index(){
-        return view('welcome');
+    public function index()
+    {
+        $all_books = Book::all();
+        
+        
+        return view('welcome', ['all_books' => $all_books]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,13 +17,23 @@ function nofunc() { // キャンセルをクリックした場合
     document.getElementById('popup').style.display = 'none';
 }
 </script>
+
+<!--<div>-->
+<!--    @foreach($all_books as $book)-->
+<!--        {{ $book->book_name }}-->
+<!--        <a href="{{ route('book.delete', ['book_id' => $book->book_id]) }}">{{ __('messages.delete') }}</a>-->
+<!--        <br>-->
+<!--    @endforeach-->
+<!--</div>-->
+
+
 @auth
-    <div >
-    <form>
-        @csrf
-        <input type="button" value="+" name="create_book_button" onclick="return popup()" />
-    </form>
-     
+    <div>
+        <form>
+            @csrf
+            <input type="button" value="+" name="create_book_button" onclick="return popup()" />
+        </form>
+    </div>
     <div id="popup" style="width: 200px;display: none;padding: 30px 20px;border: 2px solid #000;margin: auto;">
         <br />
         <form method="POST" action="{{ route('book.create') }}">
