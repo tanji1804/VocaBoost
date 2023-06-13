08e23c659c51ca0dd433af57e5f31045511da505
tanji1804 <ronkinnatsu@icloud.com>
2023-06-13 11:22:39 +0000

    #4 #5 　座標通りに表示

--- resources/views/card/image_create.blade.php
+++ resources/views/card/image_create.blade.php
@@ -2,5 +2,12 @@
 
 @section('title',config('app.name','VocaBoost').' | '. __('messages.image_create'))
 @section('content')
-    {{ $text }}
+    <div class="card" style="position: relative; width: 750px; height: 885px; background-image: url('https://storage.cloud.google.com/bucket-warm-lane-387513/IMG_7507.img.PNG');">
+        <!-- 重ねたい要素 -->
+        @foreach ($coordinates as $coordinate)
+        <div class="card-item" style="position: absolute; left: {{ $coordinate['boundingPoly']['vertices'][0]['x'] }}px; top: {{ $coordinate['boundingPoly']['vertices'][0]['y'] }}px;">
+            <h4 style="font-size: 20px; font-weight: bold; color: red;">{{ $coordinate['description'] }}</h4>
+        </div>
+        @endforeach
+    </div>
 @endsection
