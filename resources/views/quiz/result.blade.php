ef6c98223e7ec569b6a46af83691413294f10d29
tanji1804 <ronkinnatsu@icloud.com>
2023-06-23 13:49:47 +0000

    #5 result整え

--- resources/views/quiz/result.blade.php
+++ resources/views/quiz/result.blade.php
@@ -2,33 +2,49 @@
 
 @section('title',config('app.name','VocaBoost')." | ".__('messages.result'))
 @section('content')
-    @switch($type)
-        @case(0)
-            {{ __('messages.all_books') }}
-            {{ __('messages.question_from') }}
-            <a href="{{ route('quiz.question', ['type' => $type]) }}">{{ __('messages.again') }}</a>
-            @break
-        @case(1)
-            {{ __('messages.my_books') }}
-            {{ __('messages.question_from') }}
-            <a href="{{ route('quiz.question', ['type' => $type]) }}">{{ __('messages.again') }}</a>
-            @break
-        @case(2)
-            <a href="{{ route('book.index', ['id' => $book->id]) }}">{{ $book->name }}</a>
-            {{ __('messages.question_from') }}
-            <a href="{{ route('quiz.question', ['id' => $book->id, 'type' => $type]) }}">{{ __('messages.again') }}</a>
-            @break
-    @endswitch
-    <br>
-    {{ $max_points }}点中
-    {{ $points }}点
-    <br>
-    @foreach($histories as $history)
-        {{ $history->result }}
-        /
-        {{ $history->max_points }}
-        |
-        {{ $history->created_at->format('Y/m/d') }}
-        <br>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="card">
+                <div class="card-header">
+                    @switch($type)
+                        @case(0)
+                            <h2>{{ __('messages.all_books') }}</h2>
+                            <p>{{ __('messages.question_from') }}</p>
+                            <a class="btn btn-outline-info" href="{{ route('quiz.question', ['type' => $type]) }}">{{ __('messages.again') }}</a>
+                            @break
+                        @case(1)
+                            <h2>{{ __('messages.my_books') }}</h2>
+                            <p>{{ __('messages.question_from') }}</p>
+                            <a class="btn btn-outline-info" href="{{ route('quiz.question', ['type' => $type]) }}">{{ __('messages.again') }}</a>
+                            @break
+                        @case(2)
+                            <a class="h2" href="{{ route('book.index', ['id' => $book->id]) }}">{{ $book->name }}</a>
+                            <p>{{ __('messages.question_from') }}</p>
+                            <a class="btn btn-outline-info" href="{{ route('quiz.question', ['id' => $book->id, 'type' => $type]) }}">{{ __('messages.again') }}</a>
+                            @break
+                    @endswitch
+                    <a class="btn btn-outline-success" href="{{ url('/') }}">{{ __('messages.return_to_top') }}</a>
+                </div>
+                <div class="card-body">
+                    <div class="container">
+                        <div class="row">
+                            <h3>
+                                {{ $max_points }}{{ __('messages.max_points') }}
+                                {{ $points }}{{ __('messages.points') }}
+                            </h3>
+                        </div>
+                        @foreach($histories as $history)
+                            <div class="row">
+                                {{ $history->result }}
+                                /
+                                {{ $history->max_points }}
+                                |
+                                {{ $history->created_at->format('Y/m/d') }}
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
